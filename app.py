fc7216ea71c2d0186f20fe601e1ebb0688e9531b
sumitdarshanala <sumitdarshanala@gmail.com>
2024-07-17 01:14:47 +0530

    added host and port options

--- app.py
+++ app.py
@@ -44,4 +44,4 @@ def predict():
         return Response('{ "message":"Please try later"}', status=500, mimetype='application/json')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port = 5000)
