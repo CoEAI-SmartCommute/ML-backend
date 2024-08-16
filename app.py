015d2e02e6abf5e4d291010bed56dc58dec2589d
deepanshu0304 <deepanshu0322@gmail.com>
2024-08-17 02:43:28 +0530

    model updated

--- app.py
+++ app.py
@@ -2,53 +2,47 @@ from flask import Flask, Response, make_response, request, jsonify
 import numpy as np
 import pandas as pd
 from flask_cors import CORS
-from model import  calculate_point_danger, decode_polyline, get_directions
+from model import  PROJECT_ID, SYSTEM_INSTRUCT, calculate_personalized_score, decode_polyline, get_directions, time_to_section
 import requests
+import datetime
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Image
+# import os
+# from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 CORS(app)
 
-# Load the saved kNN model
-# joblib_file = "knn_model.pkl"
-# knn_model = joblib.load(joblib_file)
 
-# # Load your data that contains 'accident_score'
-# data = pd.read_csv('Data_Final.csv')
-# data['accident_score'] = (data['Death'] * 2) + (data['Grievous'] * 1.5) + (data['Minor'] * 0.5)
-
-# # We will work on improving this calculation and taking more features into consideration
-# data = data[data['accident_score'] != 0]
-# x_di = data['accident_score'].values
-
+# load_dotenv()
 @app.route('/test')
 def test():
-    # knn_model.fit()
     return Response('{ "message":"Application is up and running"}', status=201, mimetype='application/json')
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/danger_score', methods=['POST'])
 def predict():
     try :
-        # Get data from POST request
+        # print("hi1")
         data = request.get_json(force=True)
-        
-        # Parse the data for prediction
+        # print("hi2")
         longitude = data['Longitude']
         latitude = data['Latitude']
-        
-        # Create an array for prediction
-        prediction_data = np.array([[longitude, latitude]])
-        
-        # Get the nearest neighbors using the kNN model
-        # distances, indices = knn_model.kneighbors(prediction_data)
-        
-        # Calculate the danger score
-        # danger_score = calculate_point_danger(longitude, latitude, distances, indices,x_di)
-        danger_score = calculate_point_danger(prediction_data)
-        danger_score= 7-danger_score
-        danger_score= np.minimum(10,danger_score*10/7)
-        # Return the danger score as JSON
-        # return jsonify({'danger_score': danger_score})
+
+        # print(longitude)
+
+        current_time =  datetime.datetime.now()
+        # print(current_time)
+        time_section  = time_to_section(current_time)
+        # print(time_section)
+         
+        danger_score = calculate_personalized_score(latitude, longitude,None, time_section)
+
+        # danger_score= 7-danger_score
+        # danger_score= np.minimum(10,danger_score*10/7)
+
+
         response = jsonify({'danger_score': danger_score})
         return make_response(response, 200) 
     except Exception as e:
@@ -57,14 +51,9 @@ def predict():
 
 
 
-@app.route('/api/v1/test2', methods =['POST'])
-def test2():
-    data = request.get_json(force=True)
-    print(data)
-    return Response('{"message": "Deepanshu garg"}',200)
 
 
-@app.route('/api/v1/direction', methods = ['POST'])
+@app.route('/routes', methods = ['POST'])
 def getdirection():
     try:
         data = request.get_json(force=True)
@@ -73,6 +62,7 @@ def getdirection():
 
         dest_lat = str(data['dest_lat'])
         dest_long = str(data['dest_long'])
+        gender = data['gender']
 
         directions = get_directions(origin_lat, origin_long, dest_lat, dest_long)
 
@@ -90,16 +80,20 @@ def getdirection():
         
         result = []
         uid=1
+        current_time = datetime.datetime.now()
+        time_section = time_to_section(current_time)
+        print(len(direction_polylines))
         for r in direction_polylines:
             lat_long_arr = decode_polyline(r['polyline'])
             # danger = calculate_path_danger(lat_long_arr,knn_model,x_di)
             sz = len(lat_long_arr)
             danger =0
             for a in lat_long_arr:
-                to_send = []
-                to_send.append(a)
+                # to_send = []
+                # to_send.append(a)
                 # dist,ind = knn_model.kneighbors(to_send)
-                temp = calculate_point_danger(to_send)
+                # temp = calculate_point_danger(to_send)
+                temp = calculate_personalized_score(a[1], a[0], gender, time_section)
                 danger+=temp
             danger = danger/sz
             danger = 7-danger
@@ -113,6 +107,52 @@ def getdirection():
     except Exception as e:
         response = jsonify({"message": e})
         return make_response(response,500)
+    
+
+@app.route('/update_data', methods= ['POST'])
+
+def update_data():  
+        data = request.get_json(force=True)
+        lat = str(data['latitude'])
+        lng = str(data['longitude'])
+
+        date = str(data['date'])
+        time = str(data['time'])
+        description = data['description']
+
+        
+        REGION = "us-central1"
+        vertexai.init(project=PROJECT_ID, location=REGION)
+        
+
+        generative_model = GenerativeModel("gemini-1.5-pro-001")
+        response = generative_model.generate_content(
+            [SYSTEM_INSTRUCT, description])
+        print(response)
+
+        return "Deepu",200
+
+        # new_data_values = [
+        #     {'Date accident': '2024-08-01', 'Time accident': '15:30:00', 'Accident type': 'Minor Injury', 'Death': 0, 'Grievous': 0, 'Minor': 1,
+        #         'Gender': 'Male', 'Safety Device': 'Seat Belt', 'Alcohol Drugs': 'no', 'Longitude': 75.819000, 'Latitude': 11.280500, 'time_section': 'Afternoon', 'age_weightage': 0, 'accident_type_weightage': 0, 'individual_score': 0, 'accident_score':0},
+        # ]
+
+        # new_data_values['Date accident'] = date
+        # new_data_values['Time accident'] = time
+        # new_data_values['Longitude'] = lng
+        # new_data_values['Latitude']  = lat
+        # time_section = time_to_section(time)
+        # new_data_values['time_section'] = time_section
+
+
+
+        # update_data(new_data_values)
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port = 5000,debug=True)
@@ -127,6 +167,8 @@ if __name__ == '__main__':
 # origin = '40.712776,-74.005974'  # New York City
 # destination = '42.360082,-71.058880'  # Boston
 # url = f'https://maps.googleapis.com/maps/api/directions/json?origin={origin}&destination={destination}&alternatives=true&key={API_KEY}'
+
+
 
 
 
--- model.py
+++ model.py
@@ -5,41 +5,118 @@ from dotenv import load_dotenv
 import requests
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsRegressor
+from sklearn.neighbors import KNeighborsRegressor, NearestNeighbors
 import pickle
+import numpy as np
+from datetime import datetime
 
 pickle_file_path = 'array.pkl'
 
 with open(pickle_file_path, 'rb') as f:
     data = pickle.load(f)
-# data = pd.read_csv('Final_data.csv')
-# data['accident_score'] = (data['Death'] * 2) + (data['Grievous'] * 1.5) + (data['Minor'] * 0.5)
-# data = data[data['accident_score'] != 0]
-# x_di = data['accident_score'].values
-
 
 load_dotenv()
 
 # SECRET_KEY = os.getenv("API_KEY")
 
+PROJECT_ID = os.getenv("PROJECT_ID")
+SYSTEM_INSTRUCT =  """{
+    "description": "Given a user input, extract details related to accidents or crimes and map them to the appropriate dataset fields. Return the result in JSON format.",
+    # "input_example": "A death happened in Calicut of a man of 30 years old.",
+    "schema": {
+        "type": "object",
+        "properties": {
+            "incident_type": {
+                "type": "string",
+                "description": "Type of the incident, e.g., 'crime' or 'accident'."
+            },
+            "age": {
+                "type": "integer",
+                "description": "Age of the person involved in the incident."
+            },
+            "gender": {
+                "type": "string",
+                "description": "Gender of the person involved."
+            },
+            "location": {
+                "type": "object",
+                "properties": {
+                    "latitude": {"type": "number"},
+                    "longitude": {"type": "number"}
+                },
+                "description": "Location details of the incident."
+            },
+            "death": {
+                "type": "boolean",
+                "description": "True if a death occurred, false otherwise."
+            },
+            "grievous": {
+                "type": "boolean",
+                "description": "True if grievous injuries occurred, false otherwise."
+            },
+            "minor": {
+                "type": "boolean",
+                "description": "True if minor injuries occurred, false otherwise."
+            },
+            "crime_details": {
+                "type": "object",
+                "properties": {
+                    "category": {"type": "string", "description": "Category of crime."},
+                    "type_of_crime": {"type": "string", "description": "Specific type of crime, e.g., 'assault', 'robbery', 'murder'."},
+                    "score": {"type": "integer", "description": "Severity score of the crime."}
+                },
+                "description": "Details about the crime if it applies."
+            },
+            "accident_details": {
+                "type": "object",
+                "properties": {
+                    "accident_type": {"type": "string", "description": "Type of accident."},
+                    "safety_device_used": {"type": "boolean", "description": "Whether a safety device was used."},
+                    "alcohol_involvement": {"type": "boolean", "description": "True if alcohol or drugs were involved, false otherwise."},
+                    "accident_score": {"type": "integer", "description": "Severity score of the accident."}
+                },
+                "description": "Details about the accident if it applies."
+            }
+        }
+    },
+    "output_example": {
+        "incident_type": "crime",
+        "age": 30,
+        "gender": "male",
+        "location": {
+            "latitude": 11.2588,
+            "longitude": 75.7804
+        },
+        "death": True,
+        "grievous": True,
+        "minor": False,
+        "crime_details": {
+            "category": "violent crime",
+            "type_of_crime": "murder",
+            "score": 85
+        }
+    }
+}"""
 
 
-def train_model():
-#   print(data.head())
-
-  X = data[['Longitude', 'Latitude']]
-#   print(X.head())
-#   print(len(X))
-  y = data['accident_score'] 
-#   print(y.head())
-#   print(len(y))
-  k = 100
-  knn = KNeighborsRegressor(n_neighbors=k)
-  knn.fit(X, y)
-  return knn
 
 
-knn = train_model()
+# def train_model():
+# #   print(data.head())
+
+#   X = data[['Longitude', 'Latitude']]
+# #   print(X.head())
+# #   print(len(X))
+#   y = data['accident_score'] 
+# #   print(y.head())
+# #   print(len(y))
+#   k = 100
+#   knn = KNeighborsRegressor(n_neighbors=k)
+#   knn.fit(X, y)
+#   return knn
+
+
+# knn = train_model()
 
 def decode_polyline(encoded):
     if not encoded:
@@ -85,32 +162,78 @@ def decode_polyline(encoded):
 # Calculate accident score for a give lat long point
 
 
-def calculate_point_danger(prediction_data):
+# def calculate_point_danger(prediction_data):
 
-    dist, ind = knn.kneighbors(prediction_data)
-    ans = 0
-    sz = len(dist[0])
-    i = 0
-    while i < sz:
-    #   di = x_di[ind[0][i]]
-      di  = data['accident_score'].values[ind[0][i]]
-      we = 1/(200*(dist[0][i] + 0.000000000001))
-      if we > 2:  #Hyperparameter
-        we = 2
-      ans += di*we
-      i+=1
-    return ans/sz
+#     dist, ind = knn.kneighbors(prediction_data)
+#     ans = 0
+#     sz = len(dist[0])
+#     i = 0
+#     while i < sz:
+#     #   di = x_di[ind[0][i]]
+#       di  = data['accident_score'].values[ind[0][i]]
+#       we = 1/(200*(dist[0][i] + 0.000000000001))
+#       if we > 2:  #Hyperparameter
+#         we = 2
+#       ans += di*we
+#       i+=1
+#     return ans/sz
+
+def calculate_danger_score(lat, lon, filtered_data=None, k=50):
+    
+    try:
+        
+      if filtered_data is None:
+        filtered_data = data
+
+      # print("deepu")
+    
+      locations = filtered_data[['Latitude', 'Longitude']].drop_duplicates().values
+      nbrs = NearestNeighbors(
+          n_neighbors=k, algorithm='ball_tree', metric='haversine').fit(locations)
+      distances, indices = nbrs.kneighbors([[lat, lon]])
+
+      
+      # nearest_scores = filtered_data.iloc[indices[0]]['accident_score']
+      # return distances,nearest_scores
+      # print(distances)
+      ans = 0
+      sz = len(distances[0])
+      # print(sz)
+      i = 0
+      while i < sz:
+        # di = x_di[ind[0][i]]
+        di  = filtered_data['accident_score'].values[indices[0][i]]
+        # di = nearest_scores[0][i]
+        # print("Timus")
+        we = 1/(200*(distances[0][i] + 0.00000000000001))
+        if we > 2:  #Hyperparameter
+          we = 2
+        ans = ans + di*we
+        i = i+1
+      
+      # print("hi" + ans/sz)
+      return ans/sz
+    except Exception as e:
+        print(str(e) + "deepu")
+        return 0
 
 
-# def calculate_path_danger(lat_long_pair_arr):
+def calculate_personalized_score(lat, lon, gender=None, time_section=None, k=40):
+    # Filter data by gender and time section if specified
+    filtered_data = data
+    if gender:
+        # print(gender)
+        filtered_data = filtered_data[filtered_data['Gender'].str.lower() == gender.lower()]
+    if time_section:
+        # print(time_section)
+        filtered_data = filtered_data[filtered_data['time_section'] == time_section]
+    if filtered_data.empty:
+        print("No data available for the specified gender or time section.")
+        return np.nan
+    # print(filtered_data)
+    # Use filtered data to calculate danger score
+    return calculate_danger_score(lat, lon, filtered_data, k)
 
-#   sz = len(lat_long_pair_arr)
-#   total_di =0
-#   for a in lat_long_pair_arr:
-#     dist,ind = knn.kneighbors(a)
-#     temp = calculate_point_danger(a[0],a[1],dist,ind,x_di)
-#     total_di+=temp
-#   return (total_di/sz)
 
 
 def get_directions(origin_lat,origin_long,dest_lat,dest_long):
@@ -123,3 +246,95 @@ def get_directions(origin_lat,origin_long,dest_lat,dest_long):
     response = requests.get(url)
     directions = response.json()
     return directions
+
+accident_type_weightage = {
+    'Fatal': 4,
+    'Grevious Injury': 3,
+    'Minor Injury': 2,
+    'Non Injury': 1,
+    np.nan: 0  # Assign a weightage for NaN if needed
+}
+safety_device_weights = {
+    'Without wearing seatbelt': 2,
+    'Without wearing Helmet': 2,
+    'Seat Belt': 0,
+    'Wearing Helmet': 0
+}
+
+alcohol_drugs_weights = {
+    'yes': 3,
+    'no': 0,
+    'UpdateLater': 0,
+    'Yes': 3
+}
+
+
+
+def time_to_section(time_strs):
+    time_str = str(time_strs)
+    if isinstance(time_str, str):  # Ensure time_str is a string
+        try:
+            # Convert time to a datetime object
+            # print(time_str)
+            time_obj = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S.%f').time()
+            # Define time sections
+            # print(time_obj)
+            if time_obj >= datetime.strptime('07:00:00', '%H:%M:%S').time() and time_obj < datetime.strptime('19:00:00', '%H:%M:%S').time():
+                return 'Morning'
+            else:
+                return 'Night'
+        except ValueError:
+            # print("wrong2")
+            return None  
+    else:
+        # print("wrong")
+        return None  
+
+# accident_score_data['Date accident'] = pd.to_datetime(accident_score_data['Date accident'], errors='coerce')
+
+
+def calculate_age_weightage(date, interval_months=3):
+  # Ensure the date is valid
+  current_date = datetime.now()
+  if pd.isna(date):
+      return 0  # Assign zero weightage for invalid dates
+  # Calculate the difference in months between the accident date and the current date
+  months_difference = (current_date.year - date.year) *12 + current_date.month - date.month
+  # Calculate weightage: more recent dates have higher weightage
+  weightage = 1 / ((months_difference // interval_months) + 1)
+  return weightage
+
+def preprocess_and_calculate_scores():
+    # Ensure 'Date accident' is a datetime object
+    # self.data['Date accident'] = pd.to_datetime(self.data['Date accident'], errors='coerce')
+
+    # Calculate age weightage for each accident
+    data['age_weightage'] = data['Date accident'].apply(lambda x: calculate_age_weightage(x))
+
+    # Map accident type to weightages
+    data['accident_type_weightage'] = data['Accident type'].map(accident_type_weightage)
+
+    # Calculate score based on accident type weightage and age weightage
+    data['individual_score'] = (
+        (data['accident_type_weightage'] + (data['Death']* 5) + (data['Grievous']*3) + (data['Minor']*2))* data['age_weightage']
+    )
+
+    # Group by latitude and longitude
+    grouped = data.groupby(['Latitude', 'Longitude'])
+
+    # Calculate average score for each group
+    data['accident_score'] = grouped['individual_score'].transform('mean')
+
+    return data
+
+
+def update_data(new_data_values):
+    # Convert new data values to DataFrame
+    new_data = pd.DataFrame(new_data_values, columns=data.columns)
+
+    # Ensure 'Date accident' is a datetime object in the new data
+    new_data['Date accident'] = pd.to_datetime(new_data['Date accident'], errors='coerce')
+
+    # Append new data and recalculate scores
+    data = pd.concat([data, new_data], ignore_index=True)
+    preprocess_and_calculate_scores()
